c7e2b1735fcfb5e120bd9a38ab25367a36a69012
shipatulhasan <shipatulhasan>
2023-02-06 12:07:13 +0600

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='text-3xl'>Hello me</h2>
+    </div>
+  )
+}
+
+export default App
